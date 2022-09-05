fe33a41d0d677398a7c357e4c29442829d6a00f3
railya10 <railya2012@gmail.com>
2022-09-05 16:27:46 +0600

    edited

--- app/src/main/java/com/example/youtubeapi44/ui/playlist_details/PlaylistDetailsActivity.kt
+++ app/src/main/java/com/example/youtubeapi44/ui/playlist_details/PlaylistDetailsActivity.kt
@@ -2,17 +2,20 @@ package com.example.youtubeapi44.ui.playlist_details
 
 import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.example.youtubeapi44.base.BaseActivity
 import com.example.youtubeapi44.databinding.ActivityPlaylistDetailsBinding
 import com.example.youtubeapi44.ui.playlist.PlaylistActivity
 
 class PlaylistDetailsActivity() : BaseActivity <PlaylistDetailsViewModel, ActivityPlaylistDetailsBinding>() {
 
-    override val viewModel: PlaylistDetailsViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel: PlaylistDetailsViewModel by lazy {
+        ViewModelProvider(this)[PlaylistDetailsViewModel::class.java]
+    }
+
 
     override fun inflateViewBinding(inflater: LayoutInflater): ActivityPlaylistDetailsBinding {
-        TODO("Not yet implemented")
+        return ActivityPlaylistDetailsBinding.inflate(inflater)
     }
     override fun initView() {
         val channelId = intent.getStringExtra(PlaylistActivity.KEY)
